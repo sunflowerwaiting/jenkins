1eed1dcbd6316aff2670cde077555dde996a136f
Claudia（Kui） Li <claudia.li@vastaitech.com>
2021-08-11 17:18:37 +0800

    https://leetcode-cn.com/problems/remove-element/

--- insertNo.py
+++ insertNo.py
@@ -11,4 +11,28 @@ def insert(nums, target):
             else:
                 pass
 
-print(insert([1,3,4], 5))
+#print(insert([1,3,4], 5))
+
+
+def delnum(nums, val):
+    #a = len(nums)-1
+    for i ,value  in enumerate(nums):
+        if value == val:
+            nums.remove(value)
+        else:
+            pass
+    print(nums)
+    return len(nums)
+
+def removeElement(nums, val):
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)
+            else:
+                i += 1
+        print(nums)
+        return len(nums)
+#print(delnum([3,2,2,3],3))
+print(delnum([0,1,2,2,3,0,4,2],2))
+print(removeElement([0,1,2,2,3,0,4,2],2))
