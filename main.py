86fe301fb9b3e82d93b50b6202bd802e8265dedb
Claudia（Kui） Li <claudia.li@vastaitech.com>
2021-08-17 13:37:17 +0800

    change commit mesage

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import Get_commit
 import InsertExcl
-
-
-arg1 = Get_commit.get_commit('D:\GIT\jenkins')
-InsertExcl.insertExcel(arg1,'jenkins')
-
+import os
+'''
+arg1 = Get_commit.get_commit('D:\GIT\\runtime')
+InsertExcl.insertExcel(arg1,'AI_compiler')
+'''
 arg2=Get_commit.get_commit('D:\GIT\PythonLearn')
 InsertExcl.insertExcel(arg2,'PythonLearn')
