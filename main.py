264159e9b9f129580ae9944b66b1602c50a0cd80
Claudia（Kui） Li <claudia.li@vastaitech.com>
2021-08-20 10:25:31 +0800

    fix some miner bug

--- main.py
+++ main.py
@@ -19,17 +19,20 @@ work_exname = 'SW_version_release.xlsx'
 output_xls_path = work_path + work_exname
 
 if __name__ == "__main__":
-    # step 2 new work excel
+    #   step 1 clone repo
+    ComTagInsert.pull_gitlib(all_Projects,work_path)
+
+    #   step 2 new work excel
     ComTagInsert.init_excel(all_Projects, output_xls_path)
 
-    # step 3 get commit
+    #   step 3 get commit
     wb = load_workbook(output_xls_path)
     for category_name in all_Projects:
         repo_names = all_Projects[category_name]
         for repo_name in repo_names:
             repo_path = work_path + repo_name
             log_list = ComTagInsert.get_commit(repo_path, repo_name)
-            # steps 4 insert commit into excel
+            #   steps 4 insert commit into excel
             print(log_list)
             ComTagInsert.add_commit_log(log_list, wb, category_name)
     wb.save(output_xls_path)
